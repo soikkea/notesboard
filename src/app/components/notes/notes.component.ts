006b0d8b956795f8bcd28074113cf2cd9c92d135
Ari-Pekka Soikkeli <ari.soikkeli@gmail.com>
2020-09-01 18:38:58 +0300

    Add form for adding notes

--- src/app/components/notes/notes.component.ts
+++ src/app/components/notes/notes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Note } from '../../models/note'
-import { NOTES } from '../../mocks/mock-notes'
+import { Note } from '../../models/note';
+import { NoteService } from '../../services/note.service';
 
 @Component({
   selector: 'app-notes',
@@ -8,11 +8,30 @@ import { NOTES } from '../../mocks/mock-notes'
   styleUrls: ['./notes.component.css']
 })
 export class NotesComponent implements OnInit {
-  notes: Note[] = NOTES;
+  notes: Note[];
 
-  constructor() { }
+  constructor(private noteService: NoteService) { }
 
   ngOnInit(): void {
+    this.getNotes();
   }
 
+  getNotes(): void {
+    this.noteService.getNotes()
+      .subscribe(notes => this.notes = notes);
+  }
+
+  add(contents: string): void {
+    if (!contents) { return; }
+
+    const newNote: Note = {
+      content: contents,
+      date: new Date().toISOString(),
+      important: Math.random() > 0.5,
+      id: this.notes.length + 1
+    };
+    console.log('Adding a new Note!');
+    this.noteService.addNote(newNote)
+      .subscribe(note => {this.notes.push(note)})
+    };
 }
