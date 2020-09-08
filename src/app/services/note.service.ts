4d1a1cd6999df4c7cb469e45d97539e74c4c21d8
Ari-Pekka Soikkeli <ari.soikkeli@gmail.com>
2020-09-08 17:51:31 +0300

    Update NoteService to use HttpClient

--- src/app/services/note.service.ts
+++ src/app/services/note.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 
 import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
 
 import { Note } from '../models/note';
 import { NOTES } from '../mocks/mock-notes';
@@ -9,14 +11,32 @@ import { NOTES } from '../mocks/mock-notes';
   providedIn: 'root'
 })
 export class NoteService {
+  private notesUrl = 'api/notes';
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
 
   getNotes(): Observable<Note[]> {
-    return of(NOTES);
+    return this.http.get<Note[]>(this.notesUrl)
+      .pipe(
+        catchError(this.handleError<Note[]>('getNotes', []))
+      );
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      return of(result as T);
+    };
   }
 
   addNote(note: Note): Observable<Note> {
-    return of(note);
+    return this.http.post<Note>(this.notesUrl, note, this.httpOptions).pipe(
+      catchError(this.handleError<Note>('addNote'))
+    );
   }
 }
