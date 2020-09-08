7ce8a248d9563f73fdaa262f6c937876f6f10995
Ari-Pekka Soikkeli <ari.soikkeli@gmail.com>
2020-09-08 17:50:44 +0300

    Import HttpClientInMemoryWebApiModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './services/in-memory-data.service';
 
 import { AppComponent } from './app.component';
 import { NotesComponent } from './components/notes/notes.component';
@@ -15,7 +17,12 @@ import { NoteDetailComponent } from './components/note-detail/note-detail.compon
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+
+    // TODO: Remove for production use
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false}
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
