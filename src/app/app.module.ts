e45e99ffe09671e07480eafa67306862c53a8b1b
Ari-Pekka Soikkeli <ari.soikkeli@gmail.com>
2020-09-15 18:03:30 +0300

    Install Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { InMemoryDataService } from './services/in-memory-data.service';
 import { AppComponent } from './app.component';
 import { NotesComponent } from './components/notes/notes.component';
 import { NoteDetailComponent } from './components/note-detail/note-detail.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,9 @@ import { NoteDetailComponent } from './components/note-detail/note-detail.compon
     // TODO: Remove for production use
     HttpClientInMemoryWebApiModule.forRoot(
       InMemoryDataService, { dataEncapsulation: false}
-    )
+    ),
+
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
